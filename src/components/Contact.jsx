18fbd4b540863efe736201ad3c62565d564ed5ab
FeeMowa <lyonmowa@gmail.com>
2022-12-02 21:37:40 +0100

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+      <div name="contact" className='w-full h-screen  bg-[#0a192f] flex justify-center items-center p-4'>
+          <form action="" className='flex flex-col max-w-[600px] w-full'>
+              <div className='pb-8'>
+                  <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                  <p className='text-gray-300 py-4'> // Submit the form below or shoot me an email</p>
+              </div>
+              <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Your Name' name="name" />
+              <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Your Email' name="email" />
+              <textarea className='bg-[#ccd6f6] p-2' name="message" placeholder='Message' rows="10"></textarea>
+              <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-6 py-2 my-8 mx-auto flex items-center'>Submit</button>
+          </form>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Projects.jsx
@@ -0,0 +1,180 @@
+import React from "react";
+import Devshed from "../assets/devshed.jpg";
+import Chat from "../assets/chat.jpg";
+import Portfolio from "../assets/port.jpg";
+import Bookstore from "../assets/bookstore.jpg";
+import Anime from "../assets/anime-2.jpg";
+
+const Projects = () => {
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300  bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600 ">
+            Projects
+          </p>
+          <p className="py-4"> // Check out my projects</p>
+        </div>
+
+        {/* Cards Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* DevShed Project "Grid Item" */}
+          <div
+            style={{ backgroundImage: `url(${Devshed})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                DEVSHED
+              </span>
+              <div className="pt-8 text-center">
+              <a href="https://devshed.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/fidanmova/Final_Project">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    {" "}
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Chat App Project "Grid Item" */}
+          <div
+            style={{ backgroundImage: `url(${Portfolio})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                Portfolio
+              </span>
+              <div className="pt-8 text-center">
+              <a href="https://fidanportfolio.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/fidanmova/Portfolio-React">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    {" "}
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Portfolio on React  */}
+          <div
+            style={{ backgroundImage: `url(${Chat})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                Chat
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://chat-j1bt.onrender.com/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/fidanmova/Mern-Chat-App">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    {" "}
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Bookstore -4 */}
+          <div
+            style={{ backgroundImage: `url(${Bookstore})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                Bookstore
+              </span>
+              <div className="pt-8 text-center">
+              <a href="https://github.com/fidanmova/BookStore-backend">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/fidanmova/BookStore-backend">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    {" "}
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Anime*/}
+          <div
+            style={{ backgroundImage: `url(${Anime})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                Anime
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://anime-app-gamma.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/fidanmova/Anime-App">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    {" "}
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Project-6 */}
+          <div
+            // style={{ backgroundImage: `url(${Portfolio})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                DEVSHED
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    {" "}
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
