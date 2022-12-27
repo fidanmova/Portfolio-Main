4039c5354d3534f3649cfe7e23c8e6dcf399d246
FeeMowa <lyonmowa@gmail.com>
2022-12-27 12:55:09 +0100

    mailtoo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
+
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
@@ -19,22 +20,34 @@ const Navbar = () => {
       {/* Menu */}
       <ul className="hidden md:flex">
         <li>
-          <Link to='home' smooth={true} duration={500}>
+          <Link to="home" smooth={true} duration={500}>
             Home
-        </Link>
+          </Link>
         </li>
-        <li>   <Link to='about' smooth={true} duration={500}>
+        <li>
+          {" "}
+          <Link to="about" smooth={true} duration={500}>
             About
-          </Link></li>
-        <li> <Link to='skills' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="skills" smooth={true} duration={500}>
             Skills
-          </Link></li>
-        <li> <Link to='projects' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="projects" smooth={true} duration={500}>
             Projects
-          </Link></li>
-        <li>  <Link to='contact' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="contact" smooth={true} duration={500}>
             Contact
-          </Link></li>
+          </Link>
+        </li>
       </ul>
       {/* Hamburger */}
       <div onClick={handleClick} className="md:hidden z-10 cursor-pointer">
@@ -50,21 +63,37 @@ const Navbar = () => {
         }
       >
         <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
-        </Link></li>
-        <li className="py-6 text-4xl"> <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
-          </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
-          </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleClick} to='projects' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            onClick={handleClick}
+            to="projects"
+            smooth={true}
+            duration={500}
+          >
             Projects
-          </Link></li>
-        <li className="py-6 text-4xl"> <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
-          </Link></li>
+          </Link>
+        </li>
       </ul>
       {/* Social icons */}
       <div className="hidden lg:flex fixed flex-col top-[30%] left-0">
@@ -90,14 +119,12 @@ const Navbar = () => {
             </a>
           </li>
           <li className="w-[180px] h-[80px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300  bg-gray-500">
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="https://www.linkedin.com/in/fidandev/"
-            >
-              {" "}
+            <a href="mailto:fidanmova@gmail.com" target="_blank">
               Email
-              <HiOutlineMail size={30} />
             </a>
+            <HiOutlineMail size={30} />
+
+            {/* </a> */}
           </li>
           <li className="w-[180px] h-[80px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 bg-pink-600">
             <a
